4b7aafe8be36b5813c6b9ee72dbc460366d1ce39
Izubizu <39118387+Izubizu@users.noreply.github.com>
2018-09-19 12:45:47 +0200

    Add files via upload

---
+++ 1 hét/Gagyi/src/Gagyi/Gagyi.java
@@ -0,0 +1,18 @@
+package Gagyi;
+
+public class Gagyi{
+
+    
+    public static void main (String[]args)
+    {
+
+    Integer a = -129;
+    Integer b = -129;
+
+    System.out.println (a);
+    System.out.println (b);
+
+    while (a <= b && a >= b && b != a);
+
+    }
+}
